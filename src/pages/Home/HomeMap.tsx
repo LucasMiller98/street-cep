fc1304f0e0ecbde57acea90ca693610021c18a2d
Learning RPA <lucas.miller1998@outlook.com>
2021-04-25 22:47:40 -0300

    Consumindo API's

--- src/pages/Home/HomeMap.tsx
+++ src/pages/Home/HomeMap.tsx
@@ -1,5 +1,4 @@
 import { MapContainer, TileLayer, Marker, Popup  } from 'react-leaflet'
-// import Sidebar from '../../components/Sidebar' 
 import { FiLogOut } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import '../../styles/pages/home.css'
@@ -23,25 +22,25 @@ function HomeMap() {
   const [name, setName] = useState('')
   const [avatar, setAvatar] = useState('')
 
-  // useEffect(() => {
-  //   fetch(`http://api.github.com/users/lucasmiller98`)
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       setData(data)
-  //     })
+  useEffect(() => {
+    fetch(`http://api.github.com/users/lucasmiller98`)
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+      })
     
-  //   api.get(`users/lucasmiller98`)
-  //     .then(res => {
-  //       setData(res.request)
-  //     })
+    // api.get(`users/lucasmiller98`)
+    //   .then(res => {
+    //     setData(res.request)
+    //   })
 
-  // }, [])
+  }, [])
 
-  // function setData({ avatar_url }: UserFromGitHub) { 
-  //   setUserName(userName)
-  //   setName(name)
-  //   setAvatar(avatar_url)
-  // }
+  function setData({ avatar_url }: UserFromGitHub) { 
+    setUserName(userName)
+    setName(name)
+    setAvatar(avatar_url)
+  }
   
   return(
     <>
@@ -50,15 +49,14 @@ function HomeMap() {
       </Helmet>
       <div className='homeContainer'>
         <header id='header-search'>
-          {/* <input type="search" id="input-search" placeholder='Search your zip code' /> */}
+          
           <InputSearch />
         </header>
-        {/* <img src={avatar} alt="My Profile" className='profile-gitHub' />  */}
-        {/* <Sidebar /> */}
-        {/* <Link to='/login' className='exit-anchor'>
+        <img src={avatar} alt="My Profile" className='profile-gitHub' /> 
+        <Link to='/login' className='exit-anchor'>
           <span>Exit</span> 
           <FiLogOut title='Exit' size={40} color='#133499' />
-        </Link> */}
+        </Link>
        
         <MapContainer 
           center={[-8.1256917,-35.027856]} 
@@ -84,17 +82,3 @@ function HomeMap() {
 }
 
 export default HomeMap
-
-
-
-
-
-
-
-// fetch('https://api.github.com/users/lucasmiller98')
-    //   .then(res => res.json())
-    //   .then(data => console.log(data))
-
-
-     {/* <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} /> */}
-          {/* <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} /> */}
